4120f41c52b521a833ced8a8223dbce1178f4140
Kayne Ruse <kayneruse@gmail.com>
2019-08-30 00:43:59 +1000

    Updated Unity Plugin

---
+++ Assets/NinjaScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NinjaScript : MonoBehaviour {
+	Toy.ToyBehaviour behaviour = null;
+
+	void Awake() {
+		behaviour = GetComponent<Toy.ToyBehaviour>();
+	}
+
+	void Update() {
+		if (Input.GetKey("p")) {
+			//run arbitrary code at runtime
+			behaviour.Run("print \"Hello world\";");
+		}
+	}
+}
